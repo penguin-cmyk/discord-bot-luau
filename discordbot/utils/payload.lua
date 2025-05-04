61d51c108259659e2ee8e70d47e7bb0d255e35af
penguin <slomogurl37@gmail.com>
2025-05-04 11:29:45 +0200

    Update payload.lua

--- discordbot/utils/payload.lua
+++ discordbot/utils/payload.lua
@@ -32,7 +32,8 @@ payload.heartbeat_paylod = function(sequence_number)
 end 
 
 payload.get_headers = function(token)
-    return { ["Content-Type"] = "application/json", ["Authorization"] = "Bot "..token }
+    local config = bot_config.self_bot and token or ("Bot "..token)
+    return { ["Content-Type"] = "application/json", ["Authorization"] = config }
 end 
 
 payload.url_encode = function(str)
